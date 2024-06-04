6d2c64c9c9f610b564aa20ed19580d2bb4518507
jaseeerr <jaseerhasbulla01@gmail.com>
2024-06-04 13:53:28 +0400

    final111

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <nav className="bg-white  border-gray-200 dark:bg-gray-900">
            
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://res.cloudinary.com/dfethvtz3/image/upload/v1717482312/autoCargo/autoCargoLogo_r7ijup.png" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AutoCargo</span>
                 </a>
